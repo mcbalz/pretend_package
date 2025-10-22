db007b649dab174cdc69b6303c9e6357d2bb1deb
mcbalz <mcbalz@gmail.com>
2025-10-22 11:56:51 -0400

    fixed folder structure

---
+++ pretend/pretend.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Let's pretend!
+
+def pretend(imagination = False):
+    '''Looks for imagination in the argument and berates the user whether 
+    it is found or not.'''
+    if imagination:
+        print("You have imagination. Use it!")
+    else:
+        print("No imagination? Maybe try psychedelics. Or AI."
+
+if __name__ == "__main__":
+    pretend()
+
